1352393b9c861253a605392df8c80745f7ddc11f
sisko59 <6sko59@gmail.com>
2015-10-04 15:24:51 +0200

    1er test, verifie l'exception en cas d'user non loggué

--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -1,5 +1,30 @@
 package org.craftedsw.tripservicekata.trip;
 
+import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
+import org.craftedsw.tripservicekata.user.User;
+import org.junit.Test;
+
 public class TripServiceTest {
-	
+
+	private static final User GUEST = null;
+	private static final User UNUSED_USER = null;
+	private User loggedInUser;
+
+	@Test(expected = UserNotLoggedInException.class)
+	public void should_throw_exception_when_user_is_not_logged() {
+		TripService tripService = new TestableTipService();
+
+		loggedInUser = GUEST;
+
+		tripService.getTripsByUser(UNUSED_USER);
+	}
+
+	private class TestableTipService extends TripService {
+
+		@Override
+		protected User getUserLogged() {
+			return loggedInUser;
+		}
+
+	}
 }
