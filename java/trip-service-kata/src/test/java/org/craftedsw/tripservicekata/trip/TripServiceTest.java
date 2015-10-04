62e5a0c8cd4fd24922ae9d1c42e6e29fdb4b51f0
sisko59 <6sko59@gmail.com>
2015-10-04 15:39:33 +0200

    Ajout du 2eme test, pour un user logger

--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -1,13 +1,21 @@
 package org.craftedsw.tripservicekata.trip;
 
+import static org.hamcrest.core.Is.is;
+
+import java.util.List;
+
 import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
 import org.craftedsw.tripservicekata.user.User;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class TripServiceTest {
 
 	private static final User GUEST = null;
 	private static final User UNUSED_USER = null;
+	private static final User USER_LOGGED = new User();
+	private static final User ANOTHER_USER = new User();
+	private static final Trip TO_BARCELONA = new Trip();
 	private User loggedInUser;
 
 	@Test(expected = UserNotLoggedInException.class)
@@ -17,6 +25,21 @@ public class TripServiceTest {
 		loggedInUser = GUEST;
 
 		tripService.getTripsByUser(UNUSED_USER);
+	}
+
+	@Test
+	public void should_no_trip_when_user_is_not_a_friend() {
+		TripService tripService = new TestableTipService();
+
+		loggedInUser = USER_LOGGED;
+
+		User friend = new User();
+		friend.addFriend(ANOTHER_USER);
+		friend.addTrip(TO_BARCELONA);
+
+		List<Trip> friendTrips = tripService.getTripsByUser(friend);
+
+		Assert.assertThat(friendTrips.size(), is(0));
 	}
 
 	private class TestableTipService extends TripService {
